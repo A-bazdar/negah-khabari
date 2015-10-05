467c59d35fd98293c8e8195aebdefd19ba9c3ff1
Omid <omidnematollahi@outlook.com>
2015-10-05 17:10:44 +0330

    Admin template 3

--- handlers/admin.py
+++ handlers/admin.py
@@ -13,6 +13,18 @@ class AdminContentHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('admin/content_management.html')
 
+class AdminSubjectHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('admin/subject_management.html')
+
+class AdminCategoryHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('admin/category_management.html')
+
+class AdminGroupHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('admin/group_management.html')
+
 class AdminLoginHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('admin/admin_login.html')
--- urls/main_urls.py
+++ urls/main_urls.py
@@ -8,5 +8,8 @@ from handlers.admin import *
 url_patterns = [
     (r'/admin', AdminHandler),
     (r'/admin/content_management', AdminContentHandler),
+    (r'/admin/subject_management', AdminSubjectHandler),
+    (r'/admin/category_management', AdminCategoryHandler),
+    (r'/admin/group_management', AdminGroupHandler),
     (r'/admin/login', AdminLoginHandler)
 ]
