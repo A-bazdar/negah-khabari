93435277bcc3d360be9adf8efd02c58280d51305
Morteza <morteza.vatanparast@gmail.com>
2015-10-14 00:17:37 +0330

    Briefs

--- web_app/daemons/briefs.py
+++ web_app/daemons/briefs.py
@@ -19,43 +19,48 @@ def extract_briefs(document, a):
     counter = 0
     soap = BeautifulSoup(document, "html.parser")
     list_briefs = soap.select(a['brief_container'])
-    print a['base_link']
-    for i in list_briefs:
-        try:
-            if a['brief_link'] != '':
-                link = i.select_one(a['brief_link']).find('a')['href'].encode('utf-8')
-            else:
-                link = i.find('a')['href'].encode('utf-8')
-            if 'http' not in link and 'www' not in link:
-                link = a['base_link'] + link
-        except:
-            link = None
-        try:
-            if a['brief_ro_title']:
-                ro_title = i.select_one(a['brief_ro_title']).text.encode('utf-8').strip()
-            else:
+    if a['base_link'] == 'http://www.598.ir':
+        for i in list_briefs:
+            try:
+                if a['brief_link'] != '':
+                    link = i.select_one(a['brief_link']).find('a')['href'].encode('utf-8')
+                else:
+                    link = i.find('a')['href'].encode('utf-8')
+                if 'http' not in link and 'www' not in link:
+                    link = a['base_link'] + link
+            except:
+                link = None
+            try:
+                if a['brief_ro_title']:
+                    ro_title = i.select_one(a['brief_ro_title']).text.encode('utf-8').strip()
+                else:
+                    ro_title = None
+            except:
                 ro_title = None
-        except:
-            ro_title = None
-        try:
-            title = i.select_one(a['brief_title']).text.encode('utf-8').strip()
-        except:
-            title = None
-        try:
-            summary = i.select_one(a['brief_summary']).text.encode('utf-8').strip()
-        except:
-            summary = None
-        try:
-            thumbnail = i.select_one(a['brief_thumbnail']).find('img')['src']
-            if 'http' not in thumbnail and 'www' not in thumbnail:
-                thumbnail = a['base_link'] + thumbnail
-        except:
-            thumbnail = None
-        if link and title and summary and thumbnail:
-            BriefsModel(link=link, title=title, ro_title=ro_title, summary=summary, thumbnail=thumbnail, agency=str(a['id'])).insert()
-            counter += 1
-    print counter
-    print '-------------------------------------------------'
+            try:
+                title = i.select_one(a['brief_title']).text.encode('utf-8').strip()
+            except:
+                title = None
+            try:
+                summary = i.select_one(a['brief_summary']).text.encode('utf-8').strip()
+            except:
+                summary = None
+            try:
+                thumbnail = i.select_one(a['brief_thumbnail']).find('img')['src']
+                if 'http' not in thumbnail and 'www' not in thumbnail:
+                    thumbnail = a['base_link'] + thumbnail
+            except:
+                thumbnail = None
+            print link
+            print title
+            print ro_title
+            print summary
+            print thumbnail
+            if link and title and summary and thumbnail:
+                # BriefsModel(link=link, title=title, ro_title=ro_title, summary=summary, thumbnail=thumbnail, agency=str(a['id'])).insert()
+                counter += 1
+        print counter
+        print '-------------------------------------------------'
 
 
 def briefs():
