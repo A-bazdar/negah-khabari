fa0a44a8cb5667d9aa0d812ef88d516f96dd8296
Morteza <morteza.vatanparast@gmail.com>
2015-10-15 15:20:10 +0330

    News

--- web_app/daemons/news.py
+++ web_app/daemons/news.py
@@ -85,7 +85,7 @@ def news():
     briefs = BriefsModel().get_all()['value']
     for b in briefs:
         # if b['agency']['base_link'] != 'http://alef.ir' and b['agency']['base_link'] != 'http://alef.ir' and b['agency']['base_link'] != 'http://www.yjc.ir':
-        if b['agency']['base_link'] == 'http://www.mashreghnews.ir':
+        if b['agency']['base_link'] == 'http://www.baharnews.ir':
             try:
                 data = get_url(b['link'])
                 extract_news(data, b)
