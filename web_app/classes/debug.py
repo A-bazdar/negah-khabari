092a83463546a12ec090afb65f75340ac6431d06
Morteza <morteza.vatanparast@gmail.com>
2015-10-21 01:14:00 +0330

    Debug

--- web_app/classes/debug.py
+++ web_app/classes/debug.py
@@ -13,8 +13,8 @@ class Debug:
         exc_type, message, exc_tb = sys.exc_info()
         file_name, line_num, function, code = traceback.extract_tb(exc_tb)[-1]
 
-        file_name = file_name.split('/')[-1]
         file_address = file_name
+        file_name = file_name.split('/')[-1]
         if send:
             SendError(sub_system=sub_system, severity=severity, tags=tags, file_name=file_name, file_address=file_address,
                       function=function, line_num=line_num, code=code, message=message, data=data)
