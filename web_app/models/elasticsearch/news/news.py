917a8ea3094be72733391c6d1768bbc11265ceee
Morteza <morteza.vatanparast@gmail.com>
2015-10-24 19:07:21 +0330

    News

--- web_app/models/elasticsearch/news/news.py
+++ web_app/models/elasticsearch/news/news.py
@@ -71,6 +71,7 @@ class NewsModel:
             a = _source['agency'], '##'
         except:
             print _source
+            print _id
         agency = AgencyModel(_id=ObjectId(_source['agency'])).get_one()
         self.value.append(dict(
             id=_id,
