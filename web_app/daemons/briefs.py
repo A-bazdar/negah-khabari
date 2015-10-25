534cc6694917ac992d6e3f17de0e6744be9e9726
Morteza <morteza.vatanparast@gmail.com>
2015-10-25 11:19:14 +0330

    News

--- web_app/daemons/briefs.py
+++ web_app/daemons/briefs.py
@@ -35,7 +35,7 @@ def extract_briefs(document, a):
             if 'http' not in link and 'www' not in link:
                 link = a['base_link'].encode('utf-8') + link
         except:
-            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_link_brief', data=i['link'])
+            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_link_brief', data=a['base_link'])
             link = None
         try:
             if a['brief_ro_title']:
@@ -43,25 +43,24 @@ def extract_briefs(document, a):
             else:
                 ro_title = None
         except:
-            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_ro_title_brief', data=i['link'])
+            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_ro_title_brief', data=a['base_link'])
             ro_title = None
         try:
             title = i.select_one(a['brief_title']).text.encode('utf-8').strip()
         except:
-            print i
-            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_title_brief', data=i['link'])
+            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_title_brief', data=a['base_link'])
             title = None
         try:
             summary = i.select_one(a['brief_summary']).text.encode('utf-8').strip()
         except:
-            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_summary_brief', data=i['link'])
+            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_summary_brief', data=a['base_link'])
             summary = None
         try:
             thumbnail = i.select_one(a['brief_thumbnail']).find('img')['src'].encode('utf-8')
             if 'http' not in thumbnail and 'www' not in thumbnail:
                 thumbnail = a['base_link'].encode('utf-8') + thumbnail
         except:
-            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_thumbnail_brief', data=i['link'])
+            Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_thumbnail_brief', data=a['base_link'])
             thumbnail = None
         if link and title and summary and thumbnail:
             _b = BriefsModel(link=link, title=title, ro_title=ro_title, summary=summary, thumbnail=thumbnail, agency=str(a['id'])).insert()
