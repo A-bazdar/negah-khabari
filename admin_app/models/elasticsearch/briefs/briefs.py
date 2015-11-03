0eae3f895b973363179a3b5f24fd58c6e6f83a3d
Morteza <morteza.vatanparast@gmail.com>
2015-11-03 05:10:19 +0330

    ShowNews

--- admin_app/models/elasticsearch/briefs/briefs.py
+++ admin_app/models/elasticsearch/briefs/briefs.py
@@ -33,7 +33,7 @@ class BriefsModel:
 
     def get_brief(self, _source, _id):
         agency = AgencyModel(_id=ObjectId(_source['agency'])).get_one()
-        subject = SubjectModel(_id=ObjectId(_source['subject'])).get_one()['value']
+        # subject = SubjectModel(_id=ObjectId(_source['subject'])).get_one()['value']
         self.value.append(dict(
             id=_id,
             link=_source['link'],
@@ -41,7 +41,7 @@ class BriefsModel:
             ro_title=_source['ro_title'],
             summary=_source['summary'],
             thumbnail=_source['thumbnail'],
-            subject=subject,
+            # subject=subject,
             agency=agency,
             date=_source['date']
         ))
