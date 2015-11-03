f835bc8680d9c31cf2298312fb690f202e6d261d
Morteza <morteza.vatanparast@gmail.com>
2015-11-04 00:29:31 +0330

    ShowNews

--- admin_app/daemons/delete_duplicate.py
+++ admin_app/daemons/delete_duplicate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from admin_app.classes.debug import Debug
 from admin_app.handlers.base import BaseHandler
 from admin_app.models.elasticsearch.news.news import NewsModel
 
@@ -7,16 +8,20 @@ __author__ = 'Morteza'
 
 class delete_duplicate(BaseHandler):
     def get(self, *args, **kwargs):
-        all_news = []
-        for i in range(130):
-            all_news += NewsModel().get_all_all(_page=i)['value']
-        a = []
-        for i in all_news:
-            try:
-                similar = NewsModel(title=i['title'], agency=str(i['agency']['id'])).get_all_similar()['value']
-                a.append({'t': i['title'], 'c': len(similar)})
-                if len(similar) > 1:
-                    for j in similar[:-1]:
-                        print NewsModel(_id=j).delete()
-            except:
-                pass
+        try:
+            all_news = []
+            for i in range(130):
+                all_news += NewsModel().get_all_all(_page=i)['value']
+            a = []
+            for i in all_news:
+                try:
+                    similar = NewsModel(title=i['title'], agency=str(i['agency']['id'])).get_all_similar()['value']
+                    a.append({'t': i['title'], 'c': len(similar)})
+                    if len(similar) > 1:
+                        for j in similar[:-1]:
+                            print NewsModel(_id=j).delete()
+                except:
+                    Debug.get_exception(send=False)
+                    pass
+        except:
+            Debug.get_exception(send=False)
