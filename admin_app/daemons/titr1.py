6c759f43c7e7b49a300c57ec82e4bdb707f35e4a
Morteza <morteza.vatanparast@gmail.com>
2015-11-10 02:29:31 +0330

    Titr1

--- admin_app/daemons/titr1.py
+++ admin_app/daemons/titr1.py
@@ -71,8 +71,7 @@ def extract_titr1(document, a):
                 Debug.get_exception(sub_system='engine_feed', severity='error', tags='get_thumbnail_titr1',
                                     data=a['base_link'].encode('utf-8'))
                 thumbnail = None
-            print link and title and summary and thumbnail, "##########"
-            if link and title and summary and thumbnail:
+            if link is not None and title is not None and summary is not None and thumbnail is not None:
                 _b = BriefsModel(link=link, title=title, ro_title=ro_title, summary=summary, thumbnail=thumbnail,
                                  agency=str(a['id']), subject="5640dfe846b9a036ebd86e49", content="563fd1d246b9a04522af4a76").insert()
                 print _b
