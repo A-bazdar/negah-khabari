8c62cedd29105e64416bcf033c122e6f45420cd5
Morteza <morteza.vatanparast@gmail.com>
2015-11-09 22:45:37 +0330

    Titr1

--- admin_app/models/elasticsearch/news/news.py
+++ admin_app/models/elasticsearch/news/news.py
@@ -420,7 +420,7 @@ class NewsModel:
 
             r = ElasticSearchModel(index=NewsModel.index, doc_type=NewsModel.doc_type, body=body).search()
             for b in r['hits']['hits']:
-                self.value.append({'id': b['_id'], 'title': b['title']})
+                self.value.append({'id': b['_id'], 'title': b['_source']['title']})
             self.result['value'] = self.value
             self.result['status'] = True
             return self.result
