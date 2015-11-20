ff0c320d315fefe63a08f1b5f5397b4ceda7163b
Morteza <morteza.vatanparast@gmail.com>
2015-11-21 02:13:23 +0330

    EngineFeed

--- admin_app/daemons/briefs.py
+++ admin_app/daemons/briefs.py
@@ -66,7 +66,7 @@ def briefs():
     try:
         agencies = AgencyModel().get_all()['value']
         for a in agencies:
-            if a['base_link'] == "http://www.yjc.ir":
+            if a['base_link'] == "http://alef.ir":
                 for link in a['links']:
                     data = GetUrl(url=link['link']).value
                     if data:
