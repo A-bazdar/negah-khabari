c5bfc2687155527ae2c3f485eab6da2699ffee4c
Morteza <morteza.vatanparast@gmail.com>
2015-11-26 00:47:08 +0330

    BackUpElastic

--- admin_app/daemons/backup_elasticsearch.py
+++ admin_app/daemons/backup_elasticsearch.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import tarfile
 import datetime
-from admin_app.classes.debug import Debug
-from admin_app.models.elasticsearch.news.news import NewsModel
+# from admin_app.classes.debug import Debug
+# from admin_app.models.elasticsearch.news.news import NewsModel
 import json
 from admin_config import Config
 import os
@@ -13,23 +13,24 @@ __author__ = 'Morteza'
 
 
 all_news = [{'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'f': 1}]
-try:
-    backup_address_news = c.backup_elastic_search_address_news
-    # for i in range(290):
-    #     all_news += NewsModel().get_all_backup(_page=i)['value']
-    #     print len(all_news), 'news'
-    # print len(all_news)
-    with open(backup_address_news + '/news.json', 'w') as f:
-        json.dump(all_news, f)
+# try:
+backup_address_news = c.backup_elastic_search_address_news
+# for i in range(290):
+#     all_news += NewsModel().get_all_backup(_page=i)['value']
+#     print len(all_news), 'news'
+# print len(all_news)
+print backup_address_news + '/news.json'
+with open(backup_address_news + '/news.json', 'w') as f:
+    json.dump(all_news, f)
 
-    filename = str(datetime.date.today()) + '_news'
+filename = str(datetime.date.today()) + '_news'
 
-    with tarfile.open(filename, "w:gz") as tar:
-        tar.add(backup_address_news, arcname=os.path.basename(backup_address_news))
+with tarfile.open(filename, "w:gz") as tar:
+    tar.add(backup_address_news, arcname=os.path.basename(backup_address_news))
 
-except:
-    Debug.get_exception(send=False)
-    all_news = []
+# except:
+#     Debug.get_exception(send=False)
+#     all_news = []
 
 # mylist = [
 #     dict(a='a'),
