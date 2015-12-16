02538e1e741a5d7ce918ad0747aa0342b0baaa88
Omid <omid_hami@yahoo.com>
2015-12-17 02:25:35 +0330

    User styles ( Loader )

---
+++ static/plugins/loading/js/main.js
@@ -0,0 +1,14 @@
+//$(document).ready(function() {
+//
+//	setTimeout(function(){
+//		$('body').addClass('loaded');
+//		$('h1').css('color','#222222')
+//	}, 3000);
+//
+//});
+$(document).ready(function() {
+    //$(window).load(function() {
+    $('body').addClass('loaded');
+    $('h1').css('color','#222222');
+    //});
+});
