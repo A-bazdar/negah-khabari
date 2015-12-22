c22a85afeacc58907e17b54c23585531abb0a2d8
Morteza <morteza.vatanparast@gmail.com>
2015-12-22 17:23:38 +0330

    news

--- base_app/models/elasticsearch/base_model.py
+++ base_app/models/elasticsearch/base_model.py
@@ -1,5 +1,5 @@
 import json
-from base_app.models.mongodb.elastic_statistic.elastic_statistic import ElasticStatisticModel
+# from base_app.models.mongodb.elastic_statistic.elastic_statistic import ElasticStatisticModel
 from base_config import Config
 from elasticsearch import Elasticsearch
 from base_app.classes.debug import Debug
@@ -24,7 +24,7 @@ class ElasticSearchModel(ElasticSearchBaseModel):
     def search(self):
         try:
             result = self.db.search(index=self.index, doc_type=self.doc_type, body=self.body)
-            ElasticStatisticModel(index=self.index, doc_type=self.doc_type, body=self.body, result=result, function='search').insert()
+            # ElasticStatisticModel(index=self.index, doc_type=self.doc_type, body=self.body, result=result, function='search').insert()
             return result
         except:
             Debug.get_exception(sub_system='admin', severity='critical_error', tags='elastic_search > search',
@@ -34,7 +34,7 @@ class ElasticSearchModel(ElasticSearchBaseModel):
     def insert(self):
         try:
             result = self.db.index(index=self.index, doc_type=self.doc_type, id=self.id, body=self.body)
-            ElasticStatisticModel(index=self.index, doc_type=self.doc_type, item_id=self.id, body=self.body, result=result, function='insert').insert()
+            # ElasticStatisticModel(index=self.index, doc_type=self.doc_type, item_id=self.id, body=self.body, result=result, function='insert').insert()
             return result
         except:
             Debug.get_exception(sub_system='admin', severity='critical_error', tags='elastic_search > insert',
@@ -44,7 +44,7 @@ class ElasticSearchModel(ElasticSearchBaseModel):
     def delete(self):
         try:
             result = self.db.delete(index=self.index, doc_type=self.doc_type, id=self.id)
-            ElasticStatisticModel(index=self.index, doc_type=self.doc_type, item_id=self.id, result=result, function='delete').insert()
+            # ElasticStatisticModel(index=self.index, doc_type=self.doc_type, item_id=self.id, result=result, function='delete').insert()
             return True
         except:
             Debug.get_exception(sub_system='admin', severity='critical_error', tags='elastic_search > delete',
@@ -54,7 +54,7 @@ class ElasticSearchModel(ElasticSearchBaseModel):
     def update(self):
         try:
             result = self.db.update(index=self.index, doc_type=self.doc_type, id=self.id, body=self.body)
-            ElasticStatisticModel(index=self.index, doc_type=self.doc_type, item_id=self.id, body=self.body, result=result, function='update').insert()
+            # ElasticStatisticModel(index=self.index, doc_type=self.doc_type, item_id=self.id, body=self.body, result=result, function='update').insert()
             return True
         except:
             Debug.get_exception(sub_system='admin', severity='critical_error', tags='elastic_search > update',
@@ -64,7 +64,7 @@ class ElasticSearchModel(ElasticSearchBaseModel):
     def count_all(self):
         try:
             result = self.db.count(index=self.index, doc_type=self.doc_type)
-            ElasticStatisticModel(index=self.index, doc_type=self.doc_type, result=result, function='count_all').insert()
+            # ElasticStatisticModel(index=self.index, doc_type=self.doc_type, result=result, function='count_all').insert()
             return result['count']
         except:
             Debug.get_exception(sub_system='admin', severity='critical_error', tags='elastic_search > count_all',
@@ -74,7 +74,7 @@ class ElasticSearchModel(ElasticSearchBaseModel):
     def count(self):
         try:
             result = self.db.count(index=self.index, doc_type=self.doc_type, body=self.body)
-            ElasticStatisticModel(index=self.index, doc_type=self.doc_type, body=self.body, result=result, function='count').insert()
+            # ElasticStatisticModel(index=self.index, doc_type=self.doc_type, body=self.body, result=result, function='count').insert()
             return result['count']
         except:
             # Debug.get_exception(sub_system='admin', severity='critical_error', tags='elastic_search > count',
@@ -84,7 +84,7 @@ class ElasticSearchModel(ElasticSearchBaseModel):
     def get_one(self):
         try:
             result = self.db.get(index=self.index, doc_type=self.doc_type, id=self.id)
-            ElasticStatisticModel(index=self.index, doc_type=self.doc_type, item_id=self.id, result=result, function='get_one').insert()
+            # ElasticStatisticModel(index=self.index, doc_type=self.doc_type, item_id=self.id, result=result, function='get_one').insert()
             return result
         except:
             Debug.get_exception(sub_system='admin', severity='critical_error', tags='elastic_search > get_one',
