c725b4f540f10602cb918a68884b1f55f6243e4e
Morteza <morteza.vatanparast@gmail.com>
2016-01-18 04:18:08 +0330

    PermissionSearch

--- static/user/chart_content_analysis.js
+++ static/user/chart_content_analysis.js
@@ -11,28 +11,46 @@ $(document).on('click', '.chart-content-analysis.chart-content-analysis-tabs', f
 
 $(document).on('click', '#show_chart_content_analysis_modal', function (e) {
     var chart_content_analysis = $('#chart_content_analysis');
-    $('.tab_content.chart-content-analysis').hide();
-    $('.tab_content.chart-content-analysis[data-action=loader]').fadeIn();
     chart_content_analysis.modal('toggle');
     var elm = $(e.target).closest('.chart-content-analysis');
     var open = elm.attr('data-open');
-    var tab_content = $('.tab_content.chart-content-analysis[data-action=content_format]');
-    if(open == "true"){
-        $('.tab_content.chart-content-analysis').hide();
-        tab_content.fadeIn();
-    }else{
+    var action = $('.chart-content-analysis.chart-content-analysis-li.active').attr('data-action');
+    var tab_content = $('.tab_content.chart-content-analysis[data-action=' + action + ']');
+    if(open != "true"){
         jQuery.ajax(
             {
                 url: chart_content_analysis_url,
                 type: "post",
-                data: [{name: '_xsrf', value: xsrf_token}],
+                data: [{name: '_xsrf', value: xsrf_token}, {name: 'action', value: action}],
                 success: function (response) {
                     var status = response['status'];
                     var value = response['value'];
                     if (status) {
                         tab_content.html(value['html']);
-                        create_pie_chart("pie_chart_content_format", value['contents']);
-                        create_bar_chart("bar_chart_content_format", value['categories'], value['series']);
+                        if(action == "performance_agency_number_news"){
+                            create_pie_chart("pie_chart_performance_agency_number_news", value['contents']);
+                            create_bar_chart("bar_chart_performance_agency_number_news", value['categories'], value['series']);
+                        }
+                        if(action == "daily_statistics_news"){
+                            create_bar_chart("bar_chart_daily_statistics_news", value['categories'], value['series']);
+                        }
+                        if(action == "important_topic_news"){
+                            create_bar_chart_2("bar_chart_2_important_topic_news", value['categories'], value['series']);
+                        }
+                        if(action == "important_keyword_news"){
+                            create_bar_chart_2("bar_chart_2_important_keyword_news", value['categories'], value['series']);
+                        }
+                        if(action == "content_direction"){
+                            create_pie_chart("pie_chart_content_direction", value['contents']);
+                            create_bar_chart("bar_chart_content_direction", value['categories'], value['series']);
+                        }
+                        if(action == "agency_direction"){
+                            create_pie_chart("pie_chart_agency_direction", value['contents']);
+                            create_bar_chart("bar_chart_agency_direction", value['categories'], value['series']);
+                        }
+                        if(action == "important_news_maker"){
+                            create_bar_chart_2("bar_chart_2_important_news_maker", value['categories'], value['series']);
+                        }
                         $('.tab_content.chart-content-analysis').hide();
                         tab_content.fadeIn();
                         elm.attr('data-open', 'true');
