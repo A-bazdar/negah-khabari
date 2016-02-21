cc7f72a1f9876a3e606b03aa521c298bb9c332dd
Morteza <morteza.vatanparast@gmail.com>
2016-02-21 21:43:36 +0330

    SourceManagement

---
+++ base_app/scripts/scripts.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+from bson import ObjectId
+from base_app.models.mongodb.base_model import MongodbModel
+import dateutil.parser as d_parser
+
+a = open("content.json", "r")
+x = json.loads(a.read())
+for i in x:
+    i['_id'] = ObjectId(i['_id'])
+    MongodbModel(collection="content", body=i).insert()
+a.close()
+
+a = open("subject.json", "r")
+x = json.loads(a.read())
+for i in x:
+    i['_id'] = ObjectId(i['_id'])
+    try:
+        i['parent'] = ObjectId(i['parent'])
+    except:
+        i['parent'] = None
+    MongodbModel(collection="subject", body=i).insert()
+a.close()
+
+a = open("category.json", "r")
+x = json.loads(a.read())
+for i in x:
+    i['_id'] = ObjectId(i['_id'])
+    MongodbModel(collection="category", body=i).insert()
+a.close()
+
+a = open("group.json", "r")
+x = json.loads(a.read())
+for i in x:
+    i['_id'] = ObjectId(i['_id'])
+    try:
+        i['parent'] = ObjectId(i['parent'])
+    except:
+        i['parent'] = None
+    MongodbModel(collection="group", body=i).insert()
+a.close()
+
+a = open("direction.json", "r")
+x = json.loads(a.read())
+for i in x:
+    i['_id'] = ObjectId(i['_id'])
+    MongodbModel(collection="direction", body=i).insert()
+a.close()
+
+a = open("geographic.json", "r")
+x = json.loads(a.read())
+for i in x:
+    i['_id'] = ObjectId(i['_id'])
+    try:
+        i['parent'] = ObjectId(i['parent'])
+    except:
+        i['parent'] = None
+    MongodbModel(collection="geographic", body=i).insert()
+a.close()
+
+a = open("agency.json", "r")
+x = json.loads(a.read())
+for i in x:
+    i['_id'] = ObjectId(i['_id'])
+    i['direction'] = ObjectId(i['direction'])
+    i['category'] = ObjectId(i['category'])
+    try:
+        for l in i['links']:
+            l['subject'] = ObjectId(l['subject'])
+            l['group'] = ObjectId(l['group'])
+            l['content'] = ObjectId(l['content'])
+            l['geographic'] = ObjectId(l['geographic'])
+            l['start_time'] = d_parser.parse(l['start_time'])
+            l['end_time'] = d_parser.parse(l['end_time'])
+    except:
+        pass
+    try:
+        for r in i['rss_list']:
+            r['subject'] = ObjectId(r['subject'])
+            r['group'] = ObjectId(r['group'])
+            r['content'] = ObjectId(r['content'])
+            r['geographic'] = ObjectId(r['geographic'])
+            r['start_time'] = d_parser.parse(r['start_time'])
+            r['end_time'] = d_parser.parse(r['end_time'])
+    except:
+        pass
+    try:
+        for c in i['comparatives']:
+            c['subject'] = ObjectId(c['subject'])
+    except:
+        pass
+    MongodbModel(collection="agency", body=i).insert()
+a.close()
